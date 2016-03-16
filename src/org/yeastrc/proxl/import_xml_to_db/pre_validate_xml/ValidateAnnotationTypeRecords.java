ab54d4430d013909e64d54ccd8c84ab7504368d4
danjasuw <danjasuw@users.noreply.github.com>
2016-03-16 08:42:35 -0800

    Add input data validation

--- src/org/yeastrc/proxl/import_xml_to_db/pre_validate_xml/ValidateAnnotationTypeRecords.java
+++ src/org/yeastrc/proxl/import_xml_to_db/pre_validate_xml/ValidateAnnotationTypeRecords.java
@@ -21,23 +21,37 @@ import org.yeastrc.proxl_import.api.xml_dto.SearchPrograms;
 
 
 
-public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
+public class ValidateAnnotationTypeRecords {
 	
-	private static final Logger log = Logger.getLogger( ValidateAnnotationNamesUniqueWithinSearchProgramAndType.class );
+	private static final Logger log = Logger.getLogger( ValidateAnnotationTypeRecords.class );
 
-	private ValidateAnnotationNamesUniqueWithinSearchProgramAndType() { }
+	private ValidateAnnotationTypeRecords() { }
 	
-	public static ValidateAnnotationNamesUniqueWithinSearchProgramAndType getInstance() {
+	public static ValidateAnnotationTypeRecords getInstance() {
 		
-		return new ValidateAnnotationNamesUniqueWithinSearchProgramAndType();
+		return new ValidateAnnotationTypeRecords();
 	}
+	
+
+	/**
+	 * @param proxlInput
+	 * @throws ProxlImporterDataException for data errors
+	 */
+	public void validateAnnotationTypeRecords( ProxlInput proxlInput ) throws ProxlImporterDataException {
+		
+		validateAnnotationNamesUniqueWithinSearchProgramAndType( proxlInput );
+		
+		validatePresenceOfPeptideAndPSMAnnotationTypes( proxlInput );
+	}
+
+	
 
 	
 	/**
 	 * @param proxlInput
 	 * @throws ProxlImporterDataException for data errors
 	 */
-	public void validateAnnotationNamesUniqueWithinSearchProgramAndType( ProxlInput proxlInput ) throws ProxlImporterDataException {
+	private void validateAnnotationNamesUniqueWithinSearchProgramAndType( ProxlInput proxlInput ) throws ProxlImporterDataException {
 		
 
 		SearchProgramInfo searchProgramInfo = proxlInput.getSearchProgramInfo(); 
@@ -51,9 +65,9 @@ public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
 		
 		for ( SearchProgram searchProgram : searchProgramList ) {
 						
-			validateReportedPeptideAnnotationTypes( searchProgram );
+			validateReportedPeptideAnnotationNamesUniqueWithinSearchProgramAndType( searchProgram );
 
-			validatePsmAnnotationTypes( searchProgram );
+			validatePsmAnnotationNamesUniqueWithinSearchProgramAndType( searchProgram );
 		}
 		
 	}
@@ -64,7 +78,7 @@ public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
 	 * @param searchProgram
 	 * @throws ProxlImporterDataException 
 	 */
-	private void validateReportedPeptideAnnotationTypes( SearchProgram searchProgram ) throws ProxlImporterDataException {
+	private void validateReportedPeptideAnnotationNamesUniqueWithinSearchProgramAndType( SearchProgram searchProgram ) throws ProxlImporterDataException {
 		
 		Set<String> annotationNames = new HashSet<>();
 		
@@ -168,7 +182,7 @@ public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
 	 * @param searchProgram
 	 * @throws ProxlImporterDataException 
 	 */
-	private void validatePsmAnnotationTypes( SearchProgram searchProgram ) throws ProxlImporterDataException {
+	private void validatePsmAnnotationNamesUniqueWithinSearchProgramAndType( SearchProgram searchProgram ) throws ProxlImporterDataException {
 		
 		Set<String> annotationNames = new HashSet<>();
 		
@@ -216,9 +230,7 @@ public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
 						String msg = "Annotation name '" + annotationName + "'"
 								+ " occurs more than once for Psm annotation types for search program  "
 								+ "'" + searchProgram.getName() + "'.";
-
 						log.error( msg );
-
 						throw new ProxlImporterDataException( msg );
 					}
 				}
@@ -264,6 +276,152 @@ public class ValidateAnnotationNamesUniqueWithinSearchProgramAndType {
 					}
 				}
 			}
+		}
+	}
+	
+	////////////////////////////////
+	
+
+	/**
+	 * Validate presence of Peptide and PSM Annotation Types
+	 * 
+	 * Throw ProxlImporterDataException if:
+	 *   No PSM filterable annotation types.
+	 *   At least one PSM filterable  annotation type but none of them are a default filter.
+	 *   At least one Peptide filterable  annotation type but none of them are a default filter.
+	 *   
+	 * @param proxlInput
+	 * @throws ProxlImporterDataException for data errors
+	 */
+	private void validatePresenceOfPeptideAndPSMAnnotationTypes( ProxlInput proxlInput ) throws ProxlImporterDataException {
+
+		boolean foundPeptideFilterableAnnotationType = false;
+		boolean foundPeptideDefaultFilterableAnnotationType = false;
+		
+		boolean foundPsmFilterableAnnotationType = false;
+		boolean foundPsmDefaultFilterableAnnotationType = false;
+
+
+		SearchProgramInfo searchProgramInfo = proxlInput.getSearchProgramInfo(); 
+		
+		SearchPrograms proxlInputSearchPrograms = searchProgramInfo.getSearchPrograms();
+		
+		List<SearchProgram> searchProgramList =
+				proxlInputSearchPrograms.getSearchProgram();
+		
+		///////////////////////
+		
+		//   Process Peptide Annotation Types
+
+		for ( SearchProgram searchProgram : searchProgramList ) {
+
+			SearchProgram.ReportedPeptideAnnotationTypes reportedPeptideAnnotationTypes =
+					searchProgram.getReportedPeptideAnnotationTypes();
+			
+			if ( reportedPeptideAnnotationTypes == null ) {
+				
+				continue;  //  EARLY CONTINUE
+			}
+
+			//////	Filterable Peptide Annotations
+			
+			FilterablePeptideAnnotationTypes filterablePeptideAnnotationTypes =
+					reportedPeptideAnnotationTypes.getFilterablePeptideAnnotationTypes();
+
+			if ( filterablePeptideAnnotationTypes == null ) {
+				
+				continue;  //  EARLY CONTINUE
+			}
+
+			List<FilterablePeptideAnnotationType> filterablePeptideAnnotationTypeList =
+					filterablePeptideAnnotationTypes.getFilterablePeptideAnnotationType();
+
+			if ( filterablePeptideAnnotationTypeList == null || filterablePeptideAnnotationTypeList.isEmpty() ) {
+
+				continue;  //  EARLY CONTINUE
+			}
+
+			for ( FilterablePeptideAnnotationType filterablePeptideAnnotationType : filterablePeptideAnnotationTypeList ) {
+
+				foundPeptideFilterableAnnotationType = true;
+				
+				if ( filterablePeptideAnnotationType.isDefaultFilter() ) {
+
+					foundPeptideDefaultFilterableAnnotationType = true;
+				}
+				
+				break;
+			}
+		}
+		
+		///////////////////////
+		
+		//   Process PSM Annotation Types
+
+		for ( SearchProgram searchProgram : searchProgramList ) {
+
+
+			SearchProgram.PsmAnnotationTypes psmAnnotationTypes =
+					searchProgram.getPsmAnnotationTypes();
+			
+
+			if ( psmAnnotationTypes == null ) {
+				
+				continue;  //  EARLY CONTINUE
+			}
+
+			FilterablePsmAnnotationTypes filterablePsmAnnotationTypes =
+					psmAnnotationTypes.getFilterablePsmAnnotationTypes();
+
+			if ( filterablePsmAnnotationTypes == null ) {
+
+				continue;  //  EARLY CONTINUE
+			}
+
+			List<FilterablePsmAnnotationType> filterablePsmAnnotationTypeList =
+					filterablePsmAnnotationTypes.getFilterablePsmAnnotationType();
+
+			if ( filterablePsmAnnotationTypeList == null || filterablePsmAnnotationTypeList.isEmpty() ) {
+
+				continue;  //  EARLY CONTINUE
+			}
+
+			for ( FilterablePsmAnnotationType filterablePsmAnnotationType : filterablePsmAnnotationTypeList ) {
+
+				foundPsmFilterableAnnotationType = true;
+				
+				if ( filterablePsmAnnotationType.isDefaultFilter() ) {
+
+					foundPsmDefaultFilterableAnnotationType = true;
+				}
+				
+				break;
+			}
+		}
+
+		//  Not really needed since covered by XSD validation
+		if ( ! foundPsmFilterableAnnotationType ) {
+			
+			String msg = "At least one PSM Filterable Annotation Type is required.";
+			log.error( msg );
+			throw new ProxlImporterDataException( msg );
+		}
+
+		//  Not covered by XSD validation
+		if ( ! foundPsmDefaultFilterableAnnotationType ) {
+			
+			String msg = "At least one PSM Filterable Annotation Type is required to be a default filter.";
+			log.error( msg );
+			throw new ProxlImporterDataException( msg );
+		}
+		
+		//  Not covered by XSD validation
+		if ( foundPeptideFilterableAnnotationType && ( ! foundPeptideDefaultFilterableAnnotationType ) ) {
+			
+			String msg = "There is at least one Peptide Filterable Annotation Type "
+					+ " but none of theem is a default filter.";
+			log.error( msg );
+			throw new ProxlImporterDataException( msg );
 		}
 	}
 
