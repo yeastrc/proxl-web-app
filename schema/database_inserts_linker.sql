1bf33a954586e877e7001daa1118815dba9da87f
Michael Riffle <mriffle@uw.edu>
2016-03-28 14:41:39 -0700

    Updated linker creation script.

--- schema/database_inserts_linker.sql
+++ schema/database_inserts_linker.sql
@@ -4,7 +4,8 @@
 USE proxl;
 
 INSERT INTO linker(abbr,name)VALUES( 'dss','disuccinimidyl suberate' );
+INSERT INTO linker(abbr,name)VALUES( 'dsg','disuccinimidyl glutarate' );
 INSERT INTO linker(abbr,name)VALUES( 'bs3','bis[sulfosuccinimidyl] suberate' );
-
+INSERT INTO linker(abbr,name)VALUES( 'bs2','bis(sulfosuccinimidyl) glutarate' );
 INSERT INTO linker(abbr,name)VALUES( 'edc','1-ethyl-3-(3-dimethylaminopropyl)carbodiimide hydrochloride' );
 
