e7be365f302ecb002cf54666cf6bc182876efe7d
Michael Riffle <mriffle@uw.edu>
2016-06-13 16:46:34 -0700

    Started work on circle viewer.

---
+++ WebRoot/js/circle-plot/circle-plot-viewer.js
@@ -0,0 +1,29 @@
+"use strict";
+
+///////////////////////////////////////////
+
+var circlePlotViewer = function() {
+	
+	this.container = containerDiv;
+	this.initialize();
+};
+
+
+circlePlotViewer.prototype.initialize  = function(  ) {
+	
+};
+
+
+/**
+ * Draw the diagram
+ */
+circlePlotViewer.prototype.draw  = function(  ) {
+	
+};
+
+/**
+ * Draw the diagram
+ */
+circlePlotViewer.prototype.getViewerWidth  = function(  ) {
+	
+};
