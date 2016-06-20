0e29a59a1d0c9832d252b3a9d0534514551ae794
danjasuw <danjasuw@users.noreply.github.com>
2016-06-20 09:15:01 -0700

    Add database inserts for linker dfdnb

--- database_scripts/install/insert_initial_data.sql
+++ database_scripts/install/insert_initial_data.sql
@@ -36,6 +36,7 @@ INSERT INTO linker(abbr,name)VALUES( 'dsg','disuccinimidyl glutarate' );
 INSERT INTO linker(abbr,name)VALUES( 'bs3','bis[sulfosuccinimidyl] suberate' );
 INSERT INTO linker(abbr,name)VALUES( 'bs2','bis(sulfosuccinimidyl) glutarate' );
 INSERT INTO linker(abbr,name)VALUES( 'edc','1-ethyl-3-(3-dimethylaminopropyl)carbodiimide hydrochloride' );
+INSERT INTO linker(abbr,name)VALUES( 'dfdnb','1,5-difluoro-2,4-dinitrobenzene' );
 
 
 --  This will create an initial user in the "proxl" database
