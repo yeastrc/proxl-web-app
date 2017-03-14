9b0feff337ffd7938d3599f8ee7771f8c44ab96d
danjasuw <djaschob@uw.edu>
2017-03-14 13:58:12 -0700

    Run Importer - improve error handling

--- proxl_importer/src_importer_runner_pgm/org/yeastrc/proxl/import_xml_to_db_runner_pgm/get_import_and_process_thread/GetImportAndProcessThread.java
+++ proxl_importer/src_importer_runner_pgm/org/yeastrc/proxl/import_xml_to_db_runner_pgm/get_import_and_process_thread/GetImportAndProcessThread.java
@@ -12,179 +12,125 @@ import org.yeastrc.xlink.base.proxl_xml_file_import.objects.TrackingDTOTrackingR
 public class GetImportAndProcessThread extends Thread {
 
 	private static final String className = GetImportAndProcessThread.class.getSimpleName();
-
 	private static final int WAIT_TIME_TO_GET_SOMETHING_TO_PROCESS = 5; // in seconds
-
-	private static Logger log = Logger.getLogger(GetImportAndProcessThread.class);
-
-	private volatile boolean keepRunning = true;
+	private static final int WAIT_TIME_WHEN_GET_EXCEPTION = 5 * 60; // in seconds
 	
+	private static Logger log = Logger.getLogger(GetImportAndProcessThread.class);
+	
+	private volatile boolean keepRunning = true;
 	private volatile ProcessProxlXMLImport processProxlXMLImport;
 	
 	private int maxTrackingRecordPriorityToRetrieve;
-
-
+	
 	/**
 	 * default Constructor
 	 */
 	public GetImportAndProcessThread() {
-
-
 		//  Set a name for the thread
-
 		String threadName = className;
-
 		setName( threadName );
-
 		init();
 	}
-
-
 	/**
 	 * Constructor
 	 * @param s
 	 */
 	public GetImportAndProcessThread( String s ) {
-
 		super(s);
-
 		init();
 	}
-
 	/**
 	 *
 	 */
 	private void init() {
-
 	}
-
 	/**
 	 * awaken thread to get next import or to complete
 	 */
 	public void awaken() {
-
 		synchronized (this) {
-
 			notify();
 		}
-
 	}
-
-
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-
 		log.info( "run() entered" );
-
-
 		while ( keepRunning ) {
-
 			try {
-				
 				TrackingDTOTrackingRunDTOPair trackingDTOTrackingRunDTOPair =
 						GetNextTrackingToProcessDBTransaction.getInstance().getNextTrackingToProcess( maxTrackingRecordPriorityToRetrieve );
-				
 				if ( trackingDTOTrackingRunDTOPair != null ) {
-
 					synchronized (this) {
-
 						processProxlXMLImport = ProcessProxlXMLImport.getInstance();
 					}
-					
 					processProxlXMLImport.processProxlXMLImport( trackingDTOTrackingRunDTOPair );
-					
 				} else {
-
 					int waitTimeInSeconds = WAIT_TIME_TO_GET_SOMETHING_TO_PROCESS;
-
 					synchronized (this) {
-
 						try {
-
 							wait( ( (long) waitTimeInSeconds ) * 1000 ); //  wait for notify() call or timeout, in milliseconds
-
 						} catch (InterruptedException e) {
-
 							log.info("waitForSleepTime():  wait() interrupted with InterruptedException");
-
 						}
 					}
-					
 				}
-
-
 			} catch ( Throwable t ) {
+				log.error( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				log.error( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				log.error( "!!!! \n\n Exception in run(): Will next wait " + WAIT_TIME_WHEN_GET_EXCEPTION
+						+ " seconds before doing any more processing.  Exception: \n\n", t );
 
-				log.error( "Exception in run(): ", t );
+				log.error( "!!!! \n\n " );
+				log.error( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				log.error( "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
-
-				//  TODO   handle and/or log exception
-
-
-			} finally {
-				
-
+				int waitTimeInSeconds = WAIT_TIME_WHEN_GET_EXCEPTION;
 				synchronized (this) {
-
+					try {
+						wait( ( (long) waitTimeInSeconds ) * 1000 ); //  wait for notify() call or timeout, in milliseconds
+					} catch (InterruptedException e) {
+						log.info("waiting on Throwable exception caught:  wait() interrupted with InterruptedException");
+					}
+				}
+				
+			} finally {
+				synchronized (this) {
 					if ( processProxlXMLImport != null ) {
-
 						processProxlXMLImport = null;
 					}
 				}
 			}
 		}
-
-
 		log.info( "Exiting run()" );
 	}
-
-
+	
 	/**
 	 * Called on a separate thread when a shutdown request comes from the operating system.
 	 * If this is not heeded, the process may be killed by the operating system after some time has passed ( controlled by the operating system )
 	 */
 	public void shutdown() {
-
-
 		log.warn( "shutdown() called, setting keepRunning = false, calling awaken() " );
-
 		keepRunning = false;
-		
-		
 		try {
-		
 			if ( processProxlXMLImport != null ) {
-
 				processProxlXMLImport.shutdown();
 			}
-			
 		} catch ( NullPointerException e ) {
-			
 			//  Eat the NullPointerException since that meant that nothing had to be done.
 		}
-
-
 		awaken();
-
 		log.warn( "Exiting shutdown()" );
 	}
-
-
+	
 	public int getMaxTrackingRecordPriorityToRetrieve() {
 		return maxTrackingRecordPriorityToRetrieve;
 	}
-
-
 	public void setMaxTrackingRecordPriorityToRetrieve(
 			int maxTrackingRecordPriorityToRetrieve) {
 		this.maxTrackingRecordPriorityToRetrieve = maxTrackingRecordPriorityToRetrieve;
 	}
-
-
-	
-	
-
 }
--- proxl_importer/src_importer_runner_pgm/org/yeastrc/proxl/import_xml_to_db_runner_pgm/process_import/ProcessProxlXMLImport.java
+++ proxl_importer/src_importer_runner_pgm/org/yeastrc/proxl/import_xml_to_db_runner_pgm/process_import/ProcessProxlXMLImport.java
@@ -1,15 +1,12 @@
 package org.yeastrc.proxl.import_xml_to_db_runner_pgm.process_import;
 
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.yeastrc.proxl.import_xml_to_db.database_update_with_transaction_services.UpdateTrackingTrackingRunRecordsDBTransaction;
@@ -35,25 +32,16 @@ import org.yeastrc.xlink.base.proxl_xml_file_import.enum_classes.ProxlXMLFileImp
 import org.yeastrc.xlink.base.proxl_xml_file_import.objects.TrackingDTOTrackingRunDTOPair;
 import org.yeastrc.xlink.base.proxl_xml_file_import.utils.Proxl_XML_ImporterWrkDirAndSbDrsCmmn;
 
-
-
-
 /**
  * 
  *
  */
 public class ProcessProxlXMLImport {
-
 	
 	private static final String ERROR_MSG_SYSTEM_ERROR = "System Error";
-
-
 	private static final Logger log = Logger.getLogger(ProcessProxlXMLImport.class);
-	
-
 	//  private constructor
 	private ProcessProxlXMLImport() { }
-	
 	/**
 	 * @return newly created instance
 	 */
@@ -61,12 +49,9 @@ public class ProcessProxlXMLImport {
 		return new ProcessProxlXMLImport(); 
 	}
 	
-	
 	private volatile boolean shutdownRequested = false;
-	
 	private volatile RunSystemCommand runSystemCommand;
 	
-
 	/**
 	 * @param trackingDTOTrackingRunDTOPair
 	 * @throws Exception 
@@ -74,273 +59,163 @@ public class ProcessProxlXMLImport {
 	public void processProxlXMLImport( TrackingDTOTrackingRunDTOPair trackingDTOTrackingRunDTOPair ) throws Exception {
 		
 		ProxlXMLFileImportTrackingDTO proxlXMLFileImportTrackingDTO = trackingDTOTrackingRunDTOPair.getProxlXMLFileImportTrackingDTO();
-		
 		if ( log.isInfoEnabled() ) {
-
 			log.info( "Processing import for tracking id: " + proxlXMLFileImportTrackingDTO.getId() );
 		}
-		
-
 		ProxlXMLFileImportTrackingRunDTO proxlXMLFileImportTrackingRunDTO = trackingDTOTrackingRunDTOPair.getProxlXMLFileImportTrackingRunDTO();
-		
 		File proxl_XML_Importer_Work_Directory =
 				Proxl_XML_ImporterWrkDirAndSbDrsCmmn.getInstance().get_Proxl_XML_Importer_Work_Directory();
-		
 		File importerBaseDir = new File( proxl_XML_Importer_Work_Directory, ProxlXMLFileUploadCommonConstants.IMPORT_BASE_DIR );
-
 		String subdirNameForThisTrackingId =
 				Proxl_XML_ImporterWrkDirAndSbDrsCmmn.getInstance().getDirForImportTrackingId( proxlXMLFileImportTrackingDTO.getId() );
-		
 		File subdirForThisTrackingId = new File( importerBaseDir, subdirNameForThisTrackingId );
-		
 		if ( ! subdirForThisTrackingId.exists() ) {
-			
-			
 			String msg = "subdirForThisTrackingId does not exist: " + subdirForThisTrackingId.getCanonicalPath();
 			log.error( msg );
-			
 			proxlXMLFileImportTrackingRunDTO.setRunStatus( ProxlXMLFileImportStatus.FAILED );
-			
 			proxlXMLFileImportTrackingRunDTO.setRunSubStatus( ProxlXMLFileImportRunSubStatus.SYSTEM_ERROR );
-
 			proxlXMLFileImportTrackingRunDTO.setDataErrorText( ERROR_MSG_SYSTEM_ERROR );
 			proxlXMLFileImportTrackingRunDTO.setImportResultText( ERROR_MSG_SYSTEM_ERROR );
-
-
 			UpdateTrackingTrackingRunRecordsDBTransaction.getInstance()
 			.updateTrackingStatusAtImportEndupdateTrackingRunStatusResultTexts(
 					ProxlXMLFileImportStatus.FAILED, 
 					proxlXMLFileImportTrackingDTO.getId(), 
 					proxlXMLFileImportTrackingRunDTO );
-			
 			OnImprtFnshCllWbSrvc.getInstance()
 			.callProxlWebServiceOnSingleImportFinish( 
 					proxlXMLFileImportTrackingDTO.getId(), 
 					proxlXMLFileImportTrackingRunDTO.getId() );
-			
 			throw new ProxlImporterInteralException(msg);
 		}
-		
-		
-
 		final String importJarWithPath = ImporterRunnerConfigData.getImporterJarWithPath();
-		
 		//  Expecting absolute path
 		File importJarWithPathFileAloneObj = new File( importJarWithPath );
-
 		File importJarWithPathFileRelativeToTrackingDirObj = new File( subdirForThisTrackingId, importJarWithPath );
-		
 		if ( ( ! importJarWithPathFileAloneObj.exists() ) 
 				&& ( ! importJarWithPathFileRelativeToTrackingDirObj.exists() ) ) {
-
 			String errorMsg = "Import Jar with Path is not found, "
 					+ "using the subdirectory that the command will be run in as the starting point."
 					+ " Import Jar with Path in config file: "
 					+ importJarWithPath;
-			
 			log.error( errorMsg ) ;
-
 			proxlXMLFileImportTrackingRunDTO.setRunStatus( ProxlXMLFileImportStatus.FAILED );
-			
 			proxlXMLFileImportTrackingRunDTO.setRunSubStatus( ProxlXMLFileImportRunSubStatus.SYSTEM_ERROR );
-
 			proxlXMLFileImportTrackingRunDTO.setDataErrorText( ERROR_MSG_SYSTEM_ERROR );
 			proxlXMLFileImportTrackingRunDTO.setImportResultText( ERROR_MSG_SYSTEM_ERROR );
-
 			UpdateTrackingTrackingRunRecordsDBTransaction.getInstance()
 			.updateTrackingStatusAtImportEndupdateTrackingRunStatusResultTexts(
 					ProxlXMLFileImportStatus.FAILED, 
 					proxlXMLFileImportTrackingDTO.getId(), 
 					proxlXMLFileImportTrackingRunDTO );
-
 			OnImprtFnshCllWbSrvc.getInstance()
 			.callProxlWebServiceOnSingleImportFinish( 
 					proxlXMLFileImportTrackingDTO.getId(), 
 					proxlXMLFileImportTrackingRunDTO.getId() );
-			
-			
 			throw new ProxlImporterInteralException(errorMsg);
 		}
-		 
-		 
-		
 		//   Create a params file that is passed to the importer
-		
 		String runImporterParamsFilename = 
-				
 				RunImporterToImporterFilenameConstants.RUN_IMPORTER_TO_IMPORTER_FILENAME_PREFIX
 				+ proxlXMLFileImportTrackingRunDTO.getId() // Include run id as part of filename
 				+ RunImporterToImporterFilenameConstants.RUN_IMPORTER_TO_IMPORTER_FILENAME_SUFFFIX;
-
 		String importerOutputDataErrorsFilename = 
-				
 				RunImporterToImporterFilenameConstants.IMPORTER_OUTPUT_DATA_ERRORS_FILENAME_PREFIX
 				+ proxlXMLFileImportTrackingRunDTO.getId() // Include run id as part of filename
 				+ RunImporterToImporterFilenameConstants.IMPORTER_OUTPUT_DATA_ERRORS_FILENAME_SUFFFIX;
-		
-		
-		
 		RunImporterToImporterFileRoot runImporterToImporterFileRoot = new RunImporterToImporterFileRoot();
-		
 		runImporterToImporterFileRoot.setImportTrackingRunId( proxlXMLFileImportTrackingRunDTO.getId() );
-		
 		runImporterToImporterFileRoot.setProjectId( proxlXMLFileImportTrackingDTO.getProjectId() );
-		
 		runImporterToImporterFileRoot.setOutputDataErrorsFileName( importerOutputDataErrorsFilename );
-		
 //		runImporterToImporterFileRoot.setSystemInStringForShutdown( StringSendImporterToRequestShutdownConstants.SHUTDOWN );
-		
 		runImporterToImporterFileRoot.setSkipPopulatingPathOnSearch( true );
-		
 		//  Marshal (write) the object to the file
-
 		JAXBContext jaxbContext = JAXBContext.newInstance( RunImporterToImporterFileRoot.class );
-
 		Marshaller marshaller = jaxbContext.createMarshaller();
-
 		marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE );
-
 		File runImporterParamsFile = new File( subdirForThisTrackingId, runImporterParamsFilename );
-		
 		OutputStream outputStream = null;
-		
 		try {
-		
 			outputStream = new FileOutputStream( runImporterParamsFile );
-			
 			marshaller.marshal( runImporterToImporterFileRoot, outputStream );
-		
 		} catch ( Exception e ) {
-			
 			throw e;
-		
 		} finally {
-			
 			if ( outputStream != null ) {
-				
 				outputStream.close();
 			}
 		}
-		
 		String javaCommand = "java";
-		
 		if ( StringUtils.isNotEmpty( ImporterRunnerConfigData.getJavaExecutableWithPath() ) ) {
-			
 			javaCommand = ImporterRunnerConfigData.getJavaExecutableWithPath();
 		}
-	
-		
 		List<String> commandAndItsArgumentsAsList = new ArrayList<>( 20 );
-		
-		
 		commandAndItsArgumentsAsList.add( javaCommand );
-		
 		commandAndItsArgumentsAsList.add(  "-jar" );
 		commandAndItsArgumentsAsList.add( importJarWithPath );
-		
-		
 //		commandAndItsArgumentsAsList.add(  "--debug" );  //  TODO  TEMP
-		
-		
 		commandAndItsArgumentsAsList.add( 
 				"--" + RunImporterToImporterParameterNamesConstants.RUN_IMPORTER_PARAMS_FILE_PARAM_STRING 
 				+ "=" + runImporterParamsFilename );
-
 		commandAndItsArgumentsAsList.add( 
 				"--" + RunImporterToImporterParameterNamesConstants.RUN_IMPORTER_PARAMS_CREATE_FILE_ON_SUCCESS_STRING
 				+ "=" + RunImporterToImporterParameterNamesConstants.RUN_IMPORTER_PARAM_VALUE_CREATE_FILE_ON_SUCCESS_STRING );
-		
-
 		if ( ImporterRunnerConfigData.getImporterDbConfigWithPath() != null ) {
-			
 			commandAndItsArgumentsAsList.add( "-c" );
 			commandAndItsArgumentsAsList.add( ImporterRunnerConfigData.getImporterDbConfigWithPath() );
 		}
-		
-
-
 		String filenameToWriteSysoutTo = 
 				RunImporterCommandConstants.RUN_IMPORTER_COMMAND_SYSOUT_WRITTEN_TO_FILENAME_PREFIX
 				+ proxlXMLFileImportTrackingRunDTO.getId() // Include run id as part of filename
 				+ RunImporterCommandConstants.RUN_IMPORTER_COMMAND_SYSOUT_WRITTEN_TO_FILENAME_SUFFFIX;
-
 		String filenameToWriteSyserrTo = 
 				RunImporterCommandConstants.RUN_IMPORTER_COMMAND_SYSERR_WRITTEN_TO_FILENAME_PREFIX
 				+ proxlXMLFileImportTrackingRunDTO.getId() // Include run id as part of filename
 				+ RunImporterCommandConstants.RUN_IMPORTER_COMMAND_SYSERR_WRITTEN_TO_FILENAME_SUFFFIX;
-		
-		
 		File fileToWriteSysoutTo = new File( subdirForThisTrackingId, filenameToWriteSysoutTo );
 		File fileToWriteSyserrTo = new File( subdirForThisTrackingId, filenameToWriteSyserrTo );
-
 		runSystemCommand = RunSystemCommand.getInstance();
-		
 		try {
-			
 			RunSystemCommandResponse runSystemCommandResponse = 
 					runSystemCommand.runCmd( 
 							commandAndItsArgumentsAsList, 
 							subdirForThisTrackingId /* dirToRunCommandIn*/, 
 							fileToWriteSysoutTo /* fileToWriteSysoutTo*/,
 							fileToWriteSyserrTo /* fileToWriteSyserrTo*/,
-							
 							false /* throwExceptionOnCommandFailure */ );
-			
-			
 			if ( runSystemCommandResponse.isShutdownRequested() ) {
-				
-
 				log.warn( "command was aborted for run importer program shutdown: " + commandAndItsArgumentsAsList
 						+ ", subdirForThisTrackingId:  " + subdirForThisTrackingId.getCanonicalPath() );
-				
 				 proxlXMLFileImportTrackingRunDTO.setRunStatus( ProxlXMLFileImportStatus.RE_QUEUED );
-
 				 UpdateTrackingTrackingRunRecordsDBTransaction.getInstance()
 				 .updateTrackingStatusAtImportEndupdateTrackingRunStatusResultTexts(
 						 ProxlXMLFileImportStatus.RE_QUEUED, 
 						 proxlXMLFileImportTrackingDTO.getId(), 
 						 proxlXMLFileImportTrackingRunDTO );
-					
-				
 			} else {
-
 				if ( ! runSystemCommandResponse.isCommandSuccessful() ) {
-
 					log.error( "command failed: exit code: "
 							+ runSystemCommandResponse.getCommandExitCode()
 							+ ", command: "
 							+ commandAndItsArgumentsAsList
 							+ ", subdirForThisTrackingId:  " + subdirForThisTrackingId.getCanonicalPath() );
 				}
-				
-				
 				//  If importer did not update the status in DB, set to failed
-				
-				
 				ProxlXMLFileImportTrackingDTO proxlXMLFileImportTrackingDTO_AfterImporterRun =
 						ProxlXMLFileImportTracking_Base_DAO.getInstance()
 						.getItem( proxlXMLFileImportTrackingDTO.getId() );
-				
 				if ( proxlXMLFileImportTrackingDTO_AfterImporterRun.getStatus()
 						== ProxlXMLFileImportStatus.STARTED ) {
-
 					//  Status left as started so change to failed
-					
-
 					log.error( "command failed: exit code: "
 							+ runSystemCommandResponse.getCommandExitCode()
 							+ ", importer left status as 'started' so changing to 'failed' "
 							+ ", command: "
 							+ commandAndItsArgumentsAsList
 							+ ", subdirForThisTrackingId:  " + subdirForThisTrackingId.getCanonicalPath() );
-
 					 proxlXMLFileImportTrackingRunDTO.setRunStatus( ProxlXMLFileImportStatus.FAILED );
-						
 					 proxlXMLFileImportTrackingRunDTO.setRunSubStatus( ProxlXMLFileImportRunSubStatus.SYSTEM_ERROR );
-
 					 proxlXMLFileImportTrackingRunDTO.setDataErrorText( ERROR_MSG_SYSTEM_ERROR );
 					 proxlXMLFileImportTrackingRunDTO.setImportResultText( ERROR_MSG_SYSTEM_ERROR );
-
 					 UpdateTrackingTrackingRunRecordsDBTransaction.getInstance()
 					 .updateTrackingStatusAtImportEndupdateTrackingRunStatusResultTexts(
 							 ProxlXMLFileImportStatus.FAILED, 
@@ -348,42 +223,27 @@ public class ProcessProxlXMLImport {
 							 proxlXMLFileImportTrackingRunDTO );
 				}				
 			}
-			
 		} catch (Throwable e) {
-			
-
 			log.error( "command failed: " + commandAndItsArgumentsAsList
 					+ ", subdirForThisTrackingId:  " + subdirForThisTrackingId.getCanonicalPath() );
-			
 			 proxlXMLFileImportTrackingRunDTO.setRunStatus( ProxlXMLFileImportStatus.FAILED );
-
 			 proxlXMLFileImportTrackingRunDTO.setDataErrorText( ERROR_MSG_SYSTEM_ERROR );
 			 proxlXMLFileImportTrackingRunDTO.setImportResultText( ERROR_MSG_SYSTEM_ERROR );
-
 			 UpdateTrackingTrackingRunRecordsDBTransaction.getInstance()
 			 .updateTrackingStatusAtImportEndupdateTrackingRunStatusResultTexts(
 					 ProxlXMLFileImportStatus.FAILED, 
 					 proxlXMLFileImportTrackingDTO.getId(), 
 					 proxlXMLFileImportTrackingRunDTO );
-			
 			throw new Exception( e );
-			
 		} finally {
-			
 			runSystemCommand = null;
-			
-
 			OnImprtFnshCllWbSrvc.getInstance()
 			.callProxlWebServiceOnSingleImportFinish( 
 					proxlXMLFileImportTrackingDTO.getId(), 
 					proxlXMLFileImportTrackingRunDTO.getId() );
 		}
-		
-		
 		deleteUploadedFilesIfConfiguredAndStatusSuccess( proxlXMLFileImportTrackingDTO, subdirForThisTrackingId );
-		
 	}
-	
 	
 	/**
 	 * @param proxlXMLFileImportTrackingDTO
@@ -392,69 +252,40 @@ public class ProcessProxlXMLImport {
 	private void deleteUploadedFilesIfConfiguredAndStatusSuccess( ProxlXMLFileImportTrackingDTO proxlXMLFileImportTrackingDTO, File subdirForThisTrackingId ) throws Exception {
 		
 		int trackingId = proxlXMLFileImportTrackingDTO.getId();
-		
 		//  Get current record for ProxlXMLFileImportTrackingDTO since the importer pgm may have updated it.
-		
 		ProxlXMLFileImportTrackingDTO currentTrackingDTO = ProxlXMLFileImportTracking_Base_DAO.getInstance().getItem( trackingId );
-
 		if ( currentTrackingDTO.getStatus() != ProxlXMLFileImportStatus.COMPLETE ) {
-			
 			//  Status is not COMPLETE so NO DELETION
-			
 			return;  //  EARLY EXIT
 		}
-		
 		// Get configuration item
-		
 		try {
-			
 			String deleteFilesConfigValue =
 					ConfigSystemTableGetValueCommon.getInstance()
 					.getConfigValueForConfigKey( ConfigSystemsKeysSharedConstants.IMPORT_DELETE_UPLOADED_FILES );
-			
 			if ( ! ConfigSystemsValuesSharedConstants.TRUE.equals( deleteFilesConfigValue ) ) {
-
 				//  Config value in table is not true string.
-				
 				return;  //  EARLY EXIT
 			}
-			
 		} catch ( IllegalStateException e ) {
-			
 			//  Config key not in table.  Assume don't want files deleted
-			
 			return;  //  EARLY EXIT
 		}
-		
 		DeleteDirectoryAndContents.getInstance().deleteDirectoryAndContents( subdirForThisTrackingId );
-		
 	}
-	
-	
 	
 	/**
 	 * Called on a separate thread when a shutdown request comes from the operating system.
 	 * If this is not heeded, the process may be killed by the operating system after some time has passed ( controlled by the operating system )
 	 */
 	public void shutdown() {
-		
 		shutdownRequested = true;
-		
-		
 		try {
-		
 			if ( runSystemCommand != null ) {
-
 				runSystemCommand.shutdown();
 			}
-			
 		} catch ( NullPointerException e ) {
-			
 			//  Eat the NullPointerException since that meant that nothing had to be done.
 		}
-		
-		
 	}
-	
-	
 }
