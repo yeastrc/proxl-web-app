f2558a44a10fc7686c5c6cee6c2d014f44beb3fa
Dan Jaschob <djaschob@uw.edu>
2026-06-10 07:54:10 -0700

    Docker DB init config: add record: set SMTP port to 25

--- database_scripts/install/090_docker_compose.sql
+++ database_scripts/install/090_docker_compose.sql
@@ -4,6 +4,7 @@ INSERT INTO config_system (config_key, config_value) VALUES ('spectral_storage_s
 INSERT INTO config_system (config_key, config_value) VALUES ('spectral_storage_service_get_data_base_url', 'http://spectr:8080/spectral_storage_get_data');
 INSERT INTO config_system (config_key, config_value) VALUES ('email_from_address', '');
 INSERT INTO config_system (config_key, config_value) VALUES ('email_smtp_server_url', 'smtp');
+INSERT INTO config_system (config_key, config_value) VALUES ('email_smtp_server_port', '25');
 INSERT INTO config_system (config_key, config_value) VALUES ('footer_center_of_page_html', 'Proxl Docker created by Michael Riffle (<a href="mailto:mriffle@uw.edu" target="_top">mriffle@uw.edu</a>)');
 INSERT INTO config_system (config_key, config_value) VALUES ('file_import_proxl_xml_scans_temp_dir', '/data/proxl_upload');
 INSERT INTO config_system (config_key, config_value) VALUES ('scan_file_import_allowed_via_web_submit', 'true');
